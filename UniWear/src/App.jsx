ce31690bdaf21c2cbe42150fcb17b550bc80dfa4
dineth88 <dinethoshitha00@gmail.com>
2024-04-23 02:57:41 +0530

    Created Folder Structure
    Added gradient Background

---
+++ UniWear/src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <div className='gradient-background'></div>
+			<div className='white-overlay'></div>
+    </>
+  )
+}
+
+export default App
