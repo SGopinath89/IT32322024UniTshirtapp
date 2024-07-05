a1d189357ab2bbba4ab3e03eed4c565e2e0b7a36
Rukshan0 <lahirurukshan651@gmail.com>
2024-07-05 09:17:47 +0530

    Login form creation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import './Login.css';
 
 export default function Login({ setShowNav }) {
     useEffect(() => {
@@ -7,8 +8,20 @@ export default function Login({ setShowNav }) {
 
     return (
         <>
-            <div className='card'></div>
-            <h1>Login</h1>
+        <div className="login-div">
+            <div className='card login-card'>
+                <h2>Login</h2>
+                <form className="login-form">
+                    <label className="email">Email</label>
+                    <input type="email" className="form-control"/>
+                    <label className="email">Password</label>
+                    <input type="password" className="form-control"/>
+                    <button><input type="submit" className="form-control"/></button>
+                </form>
+            </div>
+        </div>
+            
+
         </>
     );
 }
