323511d977b3e2d896f667260f63c4dbbb7c38af
Charitha Dilman Lahiru <charithlahiru78@gmail.com>
2024-07-06 01:10:21 +0530

    created signup page

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,46 @@
-export default function SignUp(){
-    return(
+import { useEffect } from 'react';
+import './SignUp.css';
+
+import '../../index.css';
+
+export default function SignUp({ setShowNav }) {
+    useEffect(() => {
+        setShowNav(false);
+    }, [setShowNav]);
+
+    return (
         <>
-            <h1>Sign Up</h1>
+            <div className="login-div">
+                <div className='card login-card'>
+                    <h2>Sign Up</h2>
+                    <form className="login-form">
+                        <label className="email">First Name</label><label className="lname">Last Name</label>
+                        <input type="text" placeholder='First Name' width="30" className="FnameInput"/>
+
+                        <label className="email">Index Number</label>
+                        <input type="text" placeholder='Type your Index Number' className="form-control"/>
+
+
+                        <label className="email">Email</label>
+                        <input type="email" placeholder='Type your Email' className="form-control"/>
+
+                        <label className="email">Password</label>
+                        <input type="password"  placeholder='Type your Password' className="form-control"/>
+                        
+                        <center>
+                            <div>
+
+                                <button className='btnmargin'>Sign Up</button>
+        
+                            </div>
+                            <div>
+                                <p className='textmargin'>Already have an Account? <a href="./signup">Log In</a></p>
+                                
+                            </div>
+                        </center>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }
