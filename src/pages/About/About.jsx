05c79de6593bc5417bd6aa21379558daf6750649
Charitha Dilman Lahiru <charithlahiru78@gmail.com>
2024-07-06 12:16:05 +0530

    Updated the About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -17,16 +17,16 @@ export default function About(){
                             We help people achieve independence by making it easier to start, <br />
                             run, and grow a business. <br />
                             We believe the future of commerce has more voices, <br />
-                            not fewer, so we’re reducing the barriers to business ownership to make commerce better for everyone. <br /> <br />
+                            not fewer, so we're reducing the barriers to business ownership to make commerce better for everyone. <br /> <br />
                         </center>
 
                         <center>
                             <span className='spantopic'>OUR COMMITMENT TO SUSTAINABILITY</span><br />
-                            We’re building a 100-year company <br />
+                            We're building a 100-year company <br />
                             Shopify builds for the long term, <br />
                             and that means investing in our planet so that we can future proof <br />
                             Shopify and help our merchants future proof their businesses, too. <br />
-                            Our Sustainability Fund includes kickstarting the carbon removal market and choosing renewable energy, <br />
+                            Our Sustainability Fund includes kickstarting the carbon removal market and choosing renewable energy, <br />
                             reducing and removing our carbon emissions, and creating solutions for our merchants to do the same.
                         </center>
                     
@@ -34,5 +34,6 @@ export default function About(){
                 </div>
             </div>
         </>
+        
     );
 }
