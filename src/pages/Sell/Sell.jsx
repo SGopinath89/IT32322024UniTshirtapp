343bab5a5b1b8228a8a6f4ede193bea7aa233031
Charitha Dilman Lahiru <charithlahiru78@gmail.com>
2024-07-06 19:58:53 +0530

    updated sell page

--- src/pages/Sell/Sell.jsx
+++ src/pages/Sell/Sell.jsx
@@ -1,6 +1,9 @@
 import Img1 from '../../assets/348428189_988640645839983_6693744623780082422_n.jpg';
 import './Sell.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImage } from '@fortawesome/free-solid-svg-icons';
+
 export default function Sell(){
     return(
         <>
@@ -20,9 +23,24 @@ export default function Sell(){
                 <div className='card addItem-card'>
                     <h3>Add Your Item</h3>
 
-                    <label htmlFor="tname">T-Shirt Name</label>
-                    <label htmlFor="sdes">Short Description</label>
-                    <label htmlFor="ides">Item Description</label>
+                    <form className='form-addItem'>
+                        <label htmlFor="tname" className='txtlabel'>T-Shirt Name </label> 
+                        <input type="text" className='form-control' />
+
+                        <label htmlFor="sdes" className='txtlabel'>Short Description</label> 
+                        <input type="text" className='form-control' />
+
+                        <label htmlFor="ides" className='txtlabel'>Item Description</label> 
+                        <textarea name="txtdes" id="txtdes" cols="30" rows="10"></textarea>
+
+                        <label htmlFor="ides" className='txtlabel'>Add or Remove Feature</label>
+                        <div className='addremove-div'></div>
+
+                        <label htmlFor="ides" className='txtlabel'>Add on Images</label> <br />
+                        <FontAwesomeIcon icon={faImage}></FontAwesomeIcon>
+                        
+                    </form>
+                    
                 </div>
             </div>
         </>
