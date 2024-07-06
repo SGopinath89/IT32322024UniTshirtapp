f3a77d061fad5c0143b6e343c6b3fca4fdffe37f
LahiruParakrama <lahiruparakramadil123@gmailcom>
2024-07-06 14:30:43 +0530

    installed new package to add icons and completed all the footer jsx parts and css parts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Spline from '@splinetool/react-spline';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 import './Home.css'; // Make sure to import your CSS file
 import '../../index.css';
 import Img1 from '../../assets/348428189_988640645839983_6693744623780082422_n.jpg';
@@ -72,20 +74,36 @@ export default function Home() {
         </div>
       </div>
       <footer className="footer">
-        <div className="footer-column-section">
-          <img src={logo} alt="Logo" style={{ width: '150px', height: '30px' }}/>
-          <p> Where fashion meets academic pride.<br/>Embrace your identity with <br/>UniWear's unique designs.</p>
-        </div>
-        <div className="footer-nav">
-          <ul className="footer-nav-selection">
-            <li><NavLink to="UniWear/">Home</NavLink></li>
-            <li><NavLink to="UniWear/discover">Discover</NavLink></li>
-            <li><NavLink to="UniWear/sell">Sell</NavLink></li>
-            <li><NavLink to="UniWear/about">About</NavLink></li>
-          </ul>
-        </div>
-        <div className="footer-last-column">
-          <p>Follow UniWear</p>
+        <div className="footer-content">
+          <div className="footer-column-section">
+            <img src={logo} alt="Logo" style={{ width: '150px', height: '30px' }}/>
+            <p> Where fashion meets academic pride.<br/>Embrace your identity with <br/>UniWear's unique designs.</p>
+          </div>
+          <div className="footer-nav">
+            <ul className="footer-nav-selection">
+              <li><NavLink to="UniWear/">Home</NavLink></li>
+              <li><NavLink to="UniWear/discover">Discover</NavLink></li>
+              <li><NavLink to="UniWear/sell">Sell</NavLink></li>
+              <li><NavLink to="UniWear/about">About</NavLink></li>
+            </ul>
+          </div>
+          <div className="footer-last-column">
+            <p>Follow UniWear</p>
+              <div className="social-icons">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faFacebook} size="2x" />
+                </a>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} size="2x" />
+                </a>
+                <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+                </a>
+              </div>
+            </div>
+          </div>  
+        <div className="footer-bottom">
+          <p>Copyright©. All Rights Reserved.</p>
         </div>
       </footer>
     </>
