868684ca85e9ef03a59514ff35284dd9af578625
dineth88 <dinethoshitha00@gmail.com>
2024-07-07 03:10:48 +0530

    Revert "update app.jsx"
    This reverts commit 583969741c0e7ed77261356afdbfa56bf99eeb6f.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import Test from './pages/Test/Test';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import SignUp from './pages/SignUp/SignUp';
 import Cart from './global/components/Cart/Cart';
-import Cart from './global/components/Post/Post';
 import './App.css';
 
 function App() {
@@ -44,7 +43,6 @@ function App() {
           <Route path='/UniWear/*' element={<PageNotFound setShowNav={setShowNav} />} />
           <Route path='/UniWear/signup' element={<SignUp setShowNav={setShowNav} />} />
           <Route path='/UniWear/cart' element={<Cart setShowNav={setShowNav} />} />
-          <Route path='/UniWear/Post' element={<Post setShowNav={setShowNav} />} />
         </Routes>
       </BrowserRouter>
     </>
