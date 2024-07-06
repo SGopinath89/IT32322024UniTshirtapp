a0c838bc87e2c9cb280298a94fb8ee3594651868
dineth88 <dinethoshitha00@gmail.com>
2024-07-07 03:10:34 +0530

    Revert "update App.jsx"
    This reverts commit d2045c3e19ffb4b7a139d434d3f0df12f8800e5a.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Test from './pages/Test/Test';
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import SignUp from './pages/SignUp/SignUp';
 import Cart from './global/components/Cart/Cart';
-import Post from './global/components/Post/Post';
+import Cart from './global/components/Post/Post';
 import './App.css';
 
 function App() {
