72d1729940d57393b6bec6b5673983c19e6ee63a
dineth88 <dinethoshitha00@gmail.com>
2024-07-06 21:19:57 +0530

    Sell section

--- src/pages/Sell/Sell.jsx
+++ src/pages/Sell/Sell.jsx
@@ -24,20 +24,49 @@ export default function Sell(){
                     <h3>Add Your Item</h3>
 
                     <form className='form-addItem'>
-                        <label htmlFor="tname" className='txtlabel'>T-Shirt Name </label> 
-                        <input type="text" className='form-control' />
-
-                        <label htmlFor="sdes" className='txtlabel'>Short Description</label> 
-                        <input type="text" className='form-control' />
-
+                        <div className="name-fields">
+                            <div className="first-name">
+                                <label htmlFor="tname" className='txtlabel'>T-Shirt Name </label> 
+                                <input type="text" className='tname-form-control' />
+                            </div>
+                            <div className="last-name">
+                            <    label htmlFor="sdes" className='txtlabel'>Short Description</label> 
+                                <input type="text" className='sdes-form-control' />
+                            </div>
+                        </div>
+                        
                         <label htmlFor="ides" className='txtlabel'>Item Description</label> 
                         <textarea name="txtdes" id="txtdes" cols="30" rows="10"></textarea>
 
-                        <label htmlFor="ides" className='txtlabel'>Add or Remove Feature</label>
-                        <div className='addremove-div'></div>
+                        <label htmlFor="ides" className='txtlabel'>Add Features</label>
+                        <div className='addremove-div'>
+                            <div className="features-fields">
+                                <div className="first-feature">
+                                    <label htmlFor="tname" className='txtlabel'>Colar </label> 
+                                    <input type="text" className='tname-form-control' />
+                                    <label htmlFor="tname" className='txtlabel'>Cuff </label> 
+                                    <input type="text" className='tname-form-control' />
+                                    <label htmlFor="tname" className='txtlabel'>Placket </label> 
+                                    <input type="text" className='tname-form-control' />
+                                </div>
+                                <div className="last-feature">
+                                    <label htmlFor="sdes" className='txtlabel'>P length</label> 
+                                    <input type="text" className='sdes-form-control' />
+                                    <label htmlFor="sdes" className='txtlabel'>Material</label> 
+                                    <input type="text" className='sdes-form-control' />
+                                    <label htmlFor="sdes" className='txtlabel'>Manufacturer</label> 
+                                    <input type="text" className='sdes-form-control' />
+                                </div>
+                            </div>
+                        </div>
 
                         <label htmlFor="ides" className='txtlabel'>Add on Images</label> <br />
-                        <FontAwesomeIcon icon={faImage}></FontAwesomeIcon>
+                        <div className='img-add-div'>
+                            <div className="first-img"></div>
+                            <div className="second-img"></div>
+                            <div className="third-img"></div>
+                            <FontAwesomeIcon icon={faImage}></FontAwesomeIcon>
+                        </div>
                         
                     </form>
                     
