01b85423cfbb418522d60934b8bcc0396cb0efff
Charitha Dilman Lahiru <charithlahiru78@gmail.com>
2024-07-06 14:33:34 +0530

    created sell page

--- src/pages/Sell/Sell.jsx
+++ src/pages/Sell/Sell.jsx
@@ -1,7 +1,27 @@
+import Img1 from '../../assets/348428189_988640645839983_6693744623780082422_n.jpg';
+import './Sell.css';
+
 export default function Sell(){
     return(
         <>
-            <h1>Sell</h1>
+            <div className='txtmargin'>
+                <center>
+                    <h2>UniWear Best Sellers</h2>
+                </center>
+            </div>
+            <div className='img-div'>
+                <img src = {Img1} className='imgBestSeller'/>
+                <img src = {Img1} className='imgBestSeller'/>
+                <img src = {Img1} className='imgBestSeller'/>
+                <img src = {Img1} className='imgBestSeller'/>
+                <img src = {Img1} className='imgBestSeller'/>
+            </div>
+            <div className='addItem-div'>
+                <div className='card addItem-card'>
+                    <h2>Add Your Item</h2>
+                </div>
+                
+            </div>
         </>
     );
 }
